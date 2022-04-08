9fce7861a62473a02cc991767d6d307b6086c8e8
Vigneshwaran <vigneshwaran@divum.in>
2022-04-08 10:15:36 +0530

    added comments

--- app.js
+++ app.js
@@ -10,20 +10,25 @@ const genres =[
     {id: 3, name: 'Mystery'}
 ];
 
+// Simple Saying "Hello World" for home page
 app.get('/',(req, res) => {
     res.send('Hello World')
 });
 
+// Get all the genres
 app.get('/api/genres',(req, res) => {
     res.send(genres);
 });
 
+//Get the particular genre using its ID
 app.get('/api/genres/:id',(req, res) => {
     const genre = genres.find(gen => gen.id === parseInt(req.params.id))
     if(!genre) return res.status(404).send('Not Found');
     res.send(genre);
 });
 
+
+// Create a new genre
 app.post('/api/genres',(req, res) => {
     const {error} = validateGenre(req.body);
     if(error) return res.status(400).send(error.details[0].message);
@@ -36,7 +41,7 @@ app.post('/api/genres',(req, res) => {
     res.send(genre);
 });
 
-
+// Update the particular genre 
 app.put('/api/genres/:id',(req, res) => {
     const genre = genres.find(gen => gen.id === parseInt(req.params.id))
     if(!genre) return res.status(404).send('Not Found');
@@ -59,6 +64,7 @@ app.delete('/api/genres/:id',(req, res) =>{
 });
 
 
+// Validate the Input (Req.body)
 function validateGenre(genre){
     const schema = Joi.object({
         name : Joi.string().min(3).required(),
@@ -67,5 +73,9 @@ function validateGenre(genre){
 
 }
 
+
+// Get the System's Default port for Listen the server OR set it to 3000
 const port = process.env.PORT || 3000;
+
+// Initiate the Server
 app.listen(port,()=>console.log(`Listening on port ${port}`));
